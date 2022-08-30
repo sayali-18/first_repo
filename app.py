95e731b52bde22b43d54d8da0d72d659d824f831
sayali <spelectrical08@gmail.com>
2022-08-30 14:44:51 +0530

    master

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-#using git bash for the first time
 
-import numpy as np
-from flask import Flask
 
-print("Now I have createcd new branch new_b2 , but making chnages masterr lets check ")
+print("This is master")
--- database.py
+++ database.py
@@ -1,7 +1,1 @@
-import numpy as np
-
-import pandas as pd
-
-print("hello")
-
-print("changes for new branch new_b2")
+print("db of master")
